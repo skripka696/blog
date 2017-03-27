6ae353e4ee2912f272c4501e9a1b1586907845d9
skripka696 <skripka696@ukr.net>
2017-03-27 12:15:49 +0300

    add favorites

--- blog/user_profile/models.py
+++ blog/user_profile/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from blog.post.models import Post
 
 
 
 class  User(AbstractUser):
-    favorites = sa
+    favorites = models.ForeignKey(Post)
