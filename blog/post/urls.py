22ecb6d30900870c847f5aed0fd7d95514557fb3
skripka696 <skripka696@ukr.net>
2017-03-29 17:17:02 +0300

    add category - post  list posts list post details

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -4,4 +4,6 @@ from post import views
 
 urlpatterns = [
     url(r'^categories/$', views.CategoryView.as_view(), name='category_list'),
+    url(r'^categories/(?P<pk>\d+)/$', views.CategoryDetailView.as_view(), name='category_detail_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,7 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-from post.models import Category
+from post.models import Category, Post
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 class CategoryView(ListView):
@@ -10,7 +12,22 @@ class CategoryView(ListView):
 
     def get(self, request, *args, **kwargs):
         categories = Category.objects.all()
-        return render(request, self.template_name, {'category_list': categories})
+        return render(request, self.template_name, {'nodes': categories})
+
+
+class CategoryDetailView(ListView):
+    model = Post
+    template_name = 'category_detail_list.html'
+
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.filter(category__id=kwargs['pk'])
+        return render(request, self.template_name, {'posts': posts})
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
 
 
 class PostView(ListView):
